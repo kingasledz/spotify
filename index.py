3cf8b9b3835edae421f89eaf3d557689f9253adc
Kinga Sledz <sledz2806@gmail.com>
2023-01-16 13:16:00 +0100

    added spotify authentication

--- index.py
+++ index.py
@@ -1,17 +1,63 @@
-from flask import Flask, request, render_template, send_from_directory
+from flask import Flask, request, render_template, send_from_directory, session, redirect
+from  flask_session import Session
+import os
+import spotipy
 
 REDIRECT_URL = 'http://127.0.0.1:5000/auth'
-
+CLIENT_ID = "***REMOVED***"
+CLIENT_SECRET = "***REMOVED***"
+# python3 -m venv .venv
+# source .venv/bin/activate
 app = Flask(__name__)
+
+
+
+
+app.config['SECRET_KEY'] = os.urandom(64)
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = './.flask_session/'
+Session(app)
+
 
 # Debug setting set to true
 app.debug = True
 
+
+
 @app.route('/')
 def index():
-    # serve index.html
-    # return render_template('index.html')
+    # cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    # auth_manager = spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,
+    #                                            client_secret=CLIENT_SECRET,
+    #                                            redirect_uri=REDIRECT_URL,
+    #                                            scope='playlist-modify-private',
+    #                                            cache_handler=cache_handler,
+    #                                            show_dialog=True)
     return app.send_static_file('index.html')
+    
+
+@app.route('/login')
+def login():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URL,
+                                               scope='playlist-modify-private',
+                                               cache_handler=cache_handler)
+    auth_url = auth_manager.get_authorize_url()
+
+    session["date"] = request.args.get("date")
+
+    return render_template('login.html', auth_url=auth_url)
+        
+
+
+
+
+@app.route('/get/')
+def get():
+    return session.get('key', 'not set')
+
 
 # @app.route('/start')
 # def start():
@@ -37,17 +83,42 @@ def index():
 
 @app.route('/auth')
 def auth():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URL,
+                                               scope='playlist-modify-private',
+                                               cache_handler=cache_handler)
+    auth_manager.get_access_token(request.args.get("code"))
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/login')
+
+    return redirect('/playlist')
     # receive code
     # exchange code for token
     # read date from front-end
-    return "hello world"
 
 @app.route('/playlist')
 def playlist():
+    cache_handler = spotipy.cache_handler.FlaskSessionCacheHandler(session)
+    auth_manager = spotipy.oauth2.SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URL,
+                                               scope='playlist-modify-private',
+                                               cache_handler=cache_handler)
+
+    if not auth_manager.validate_token(cache_handler.get_cached_token()):
+        return redirect('/login')
+
+    date_now = session.get('date', None)
+
+    return "hello" + date_now
+    
     # run magic
     # serve done.html
     # handle errors
-    pass
+    
 
 @app.route('/<path:path>')
 def static_files(path):
